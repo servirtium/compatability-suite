44ee201f27fed873eadc9d32e896b10637014629
Paul Hammant <paul@hammant.org>
2022-08-21 10:54:43 +0100

    clearer

--- js/specs.js
+++ js/specs.js
@@ -278,16 +278,16 @@ function defineSpecsFor(apiRoot){
   function ajax(httpMethod, url, options, expectedSC) {
 
     if (url.startsWith("https://localhost:61417")) {
-      throw new Error("All communication for the Servirtium compatibility test suite should go through Servrtium\n" +
+      throw new Error("All communication for the Servirtium compatibility test suite should go through Servirtium\n" +
           "on http://localhost:61417, yet something in the headers or body of a prior request is\n " +
-          "implicating https://localhost:61417 as the server, yet it is not up on https it is listening\n " +
+          "implicating https://localhost:61417 as the URL of the server, yet it is not up on https it is listening\n " +
           "on plain http - the **mutation** of a prior response to the localhost form of the URL isn't\n " +
           "correct.");
     }
 
     if (! url.startsWith("http://localhost:61417")) {
       const domain = url.substring(0, url.lastIndexOf("/todo"));
-      throw new Error("All communication for the Servirtium compatability test suite should go through Servrtium\n" +
+      throw new Error("All communication for the Servirtium compatability test suite should go through Servirtium\n" +
           "on http://localhost:61417, yet something in the headers or body of a prior response was accessing\n" + domain + " incorrectly. To fix this:\n" +
           domain + " should have been **mutated** into http://localhost:61417 and \n" +
           domain.substring(domain.indexOf("//")+2) + " should have been mutated into localhost:61417 in prior response headers and bodies,");
